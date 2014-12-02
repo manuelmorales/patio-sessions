7cc57270a2906ba52d4a3e7bf07752d4a9bd91c7
Manuel Morales <manuelmorales@gmail.com>
2014-12-02 05:42:30 +0000

    Using RSpec rack to test the controller

--- spec/patio_sessions/sessions_controller_spec.rb
+++ spec/patio_sessions/sessions_controller_spec.rb
@@ -4,6 +4,12 @@ describe SessionsController do
   context 'Show' do
     let(:path) { "/sessions/#{session_id}" }
     let(:session_id) { 'a-session-id' }
+    let(:session) { double('session', id: session_id) }
+    let(:sessions_repo) { double('sessions_repo', find: session) }
+
+    before do
+      allow(subject).to receive(:sessions_repo).and_return(sessions_repo)
+    end
 
     context 'Rack integration' do
       let(:app){ RackApp }
@@ -23,55 +29,18 @@ describe SessionsController do
         expect(last_response.status).to eq 200
         expect(last_response.content_type).to eq('application/json')
       end
-    end
 
-    context 'successful' do
-      subject{ SessionsController::Show.new }
 
-      def response value = nil
-        @response ||= value
-      end
+      context 'successful' do
+        it 'returns status 200' do
+          do_request
+          expect(last_response.status).to eq 200
+        end
 
-      def rack_response response = @response
-        Rack::Response.new response[2], response[0], response[1]
-      end
-
-      def body response = @response
-        JSON.parse rack_response(response).body.join, :symbolize_names => true
-      end
-
-      def status response = @response
-        rack_response(response).status
-      end
-
-      def headers response = @response
-        rack_response(response).headers
-      end
-
-      def header name, response = @response
-        headers(response)[name]
-      end
-
-      it 'returns status 200' do
-        response subject.call('router.params' => {id: session_id})
-        expect(status).to eq 200
-      end
-
-      it 'returns valid JSON' do
-        response subject.call('router.params' => {id: session_id})
-        expect(header 'Content-Type').to eq 'application/json'
-        expect(body).to be_a Hash
-      end
-
-      let(:session) { double('session', id: session_id) }
-      let(:sessions_repo) { double('sessions_repo', find: session) }
-      before do
-        allow(subject).to receive(:sessions_repo).and_return(sessions_repo)
-      end
-
-      it 'gets the session from the session repository' do
-        response subject.call('router.params' => {id: session_id})
-        expect(body).to eq(id: session_id)
+        it 'renders the session from the session repository' do
+          do_request
+          expect(JSON.parse last_response.body).to eq('id' => session_id)
+        end
       end
     end
   end
