5ae3299279ba73b3c4964d2fc56ad4e76e6ce725
Manuel Morales <manuelmorales@gmail.com>
2014-12-07 16:28:43 +0000

    Provided cattr_injectable

--- lib/injectable.rb
+++ lib/injectable.rb
@@ -2,31 +2,52 @@ module Injectable
   NULL = Object.new
 
   def self.included klass
-    klass.extend ClassMethods
+    klass.extend self
   end
 
-  module ClassMethods
-    def injectable name
-      define_method name do |value = NULL, &block|
-        if block
-          if value == NULL || value == nil
-            instance_variable_set("@#{name}_proc", block)
-          else
-            send("#{name}=", value)
-          end
+  def cattr_injectable name
+    define_singleton_method name do |value = NULL, &block|
+      if block
+        if value == NULL || value == nil
+          instance_variable_set("@#{name}_proc", block)
         else
-          if value == NULL
-            instance_variable_get("@#{name}_proc").call
-          else
-            send("#{name}=", value)
-          end
+          send("#{name}=", value)
+        end
+      else
+        if value == NULL
+          instance_variable_get("@#{name}_proc").call
+        else
+          send("#{name}=", value)
         end
       end
+    end
 
-      define_method "#{name}=" do |value|
-        instance_variable_set("@#{name}_proc", lambda{ value })
-        value
+    define_singleton_method "#{name}=" do |value|
+      instance_variable_set("@#{name}_proc", lambda{ value })
+      value
+    end
+  end
+
+  def attr_injectable name
+    define_method name do |value = NULL, &block|
+      if block
+        if value == NULL || value == nil
+          instance_variable_set("@#{name}_proc", block)
+        else
+          send("#{name}=", value)
+        end
+      else
+        if value == NULL
+          instance_variable_get("@#{name}_proc").call
+        else
+          send("#{name}=", value)
+        end
       end
+    end
+
+    define_method "#{name}=" do |value|
+      instance_variable_set("@#{name}_proc", lambda{ value })
+      value
     end
   end
 end
