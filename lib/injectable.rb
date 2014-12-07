2d2e053b0eee13656f4aeb74838296005ef2810c
Manuel Morales <manuelmorales@gmail.com>
2014-12-07 16:37:19 +0000

    Refactored cattr_injectable

--- lib/injectable.rb
+++ lib/injectable.rb
@@ -5,8 +5,8 @@ module Injectable
     klass.extend self
   end
 
-  def cattr_injectable name
-    define_singleton_method name do |value = NULL, &block|
+  def self.getsetter_definition_for name
+    lambda do |value = NULL, &block|
       if block
         if value == NULL || value == nil
           instance_variable_set("@#{name}_proc", block)
@@ -21,33 +21,22 @@ module Injectable
         end
       end
     end
+  end
 
-    define_singleton_method "#{name}=" do |value|
+  def self.setter_definition_for name
+    lambda do |value|
       instance_variable_set("@#{name}_proc", lambda{ value })
       value
     end
   end
 
-  def attr_injectable name
-    define_method name do |value = NULL, &block|
-      if block
-        if value == NULL || value == nil
-          instance_variable_set("@#{name}_proc", block)
-        else
-          send("#{name}=", value)
-        end
-      else
-        if value == NULL
-          instance_variable_get("@#{name}_proc").call
-        else
-          send("#{name}=", value)
-        end
-      end
-    end
+  def cattr_injectable name
+    define_singleton_method name, &Injectable.getsetter_definition_for(name)
+    define_singleton_method "#{name}=", &Injectable.setter_definition_for(name)
+  end
 
-    define_method "#{name}=" do |value|
-      instance_variable_set("@#{name}_proc", lambda{ value })
-      value
-    end
+  def attr_injectable name
+    define_method name, &Injectable.getsetter_definition_for(name)
+    define_method "#{name}=", &Injectable.setter_definition_for(name)
   end
 end
