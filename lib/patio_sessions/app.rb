ee88950ccf30e6840ae90f3e68d9ccf98456dad4
Manuel Morales <manuelmorales@gmail.com>
2014-12-08 23:35:08 +0000

    Using let and section

--- lib/patio_sessions/app.rb
+++ lib/patio_sessions/app.rb
@@ -1,19 +1,27 @@
-require 'hashie'
-
+require 'hashie' 
 module PatioSessions
-  class App
+  class AppBase
+    def let name, &block
+      define_singleton_method "#{name}=" do |value|
+        eval "@#{name} = value"
+      end
+
+      define_singleton_method name do
+        eval "@#{name} ||= block.call"
+      end
+    end
+
+    def section name, &block
+      let name do
+        self.class.new.tap { |a| block.call a }
+      end
+    end
+
     def actions
       @actions ||= Hashie::Mash.new.tap do |h|
         h.sessions!.show = SessionsController::Show
         h.sessions!.show.sessions_repo = Resolver.new { repos.sessions }
         h.sessions!.show.not_found_exception { exceptions.not_found }
-      end
-    end
-
-    def repos
-      @repos ||= Hashie::Mash.new.tap do |h|
-        h.sessions = SessionsMemoryRepo.new
-        h.sessions.not_found_exception { exceptions.not_found }
       end
     end
 
@@ -36,6 +44,22 @@ module PatioSessions
           end
         end
       end
+    end
+  end
+
+  class App
+    def self.new
+      app = AppBase.new
+
+      app.section :repos do |repos|
+        repos.let :sessions do
+          SessionsMemoryRepo.new.tap do |r|
+            r.not_found_exception { app.exceptions.not_found }
+          end
+        end
+      end
+
+      app
     end
   end
 end
