325489f1c2e9b15f55d47804208bd892b02c2259
Manuel Morales <manuelmorales@gmail.com>
2014-12-13 23:20:19 +0000

    Show action refactored

--- lib/patio_sessions/sessions_controller.rb
+++ lib/patio_sessions/sessions_controller.rb
@@ -2,76 +2,6 @@ module PatioSessions
   class SessionsController
     require 'injectable'
 
-    class Show
-      include Injectable
-
-      attr_injectable :not_found_exception
-      attr_injectable :sessions_repo
-      attr_injectable :serializer
-
-      def call env
-        @env = env
-        begin
-          action
-          render
-        rescue not_found_exception => e
-          message = "Could not find session with id #{e.id}"
-          body error_code: :not_found, error_message: message
-          status 404
-          render
-        end
-      end
-
-      private
-
-      def action
-        session = sessions_repo.find(session_id)
-        body serializer.session(session)
-      end
-
-      def session_id
-        env['router.params'][:id]
-      end
-
-      NULL = Object.new
-
-      def body value = NULL
-        if value == NULL
-          @body
-        else
-          @body = value
-        end
-      end
-
-      def header key, value = NULL
-        if value == NULL
-          headers[key]
-        else
-          headers[key] = value
-        end
-      end
-
-      def headers
-        @headers ||= {'Content-Type' => 'application/json'}
-      end
-
-      def status value = NULL
-        if value == NULL
-          @status ||= 200
-        else
-          @status = value
-        end
-      end
-
-      def render
-        [status, headers, [body.to_json]]
-      end
-
-      def env
-        @env
-      end
-    end
-
     module Base
       module ClassMethods
         def call env
@@ -141,6 +71,42 @@ module PatioSessions
 
       def env
         @env
+      end
+    end
+
+    class Show
+      include Base
+      
+      cattr_injectable :not_found_exception
+      def_delegator :'self.class', :not_found_exception
+
+      cattr_injectable :serializer
+      def_delegator :'self.class', :serializer
+
+      def call env
+        @env = env
+        header 'Content-Type', 'application/json'
+
+        begin
+          action
+          render
+        rescue not_found_exception => e
+          message = "Could not find session with id #{e.id}"
+          body error_code: :not_found, error_message: message
+          status 404
+          render
+        end
+      end
+
+      private
+
+      def action
+        session = sessions_repo.find(session_id)
+        body serializer.session(session)
+      end
+
+      def session_id
+        env['router.params'][:id]
       end
     end
 
