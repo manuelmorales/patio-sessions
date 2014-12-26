54e43686bc999116c59a485bef3bb9ee7248e1e2
Manuel Morales <manuelmorales@gmail.com>
2014-12-26 23:36:36 +0000

    Section is no longer needed

--- spec/patio_sessions/app_spec.rb
+++ spec/patio_sessions/app_spec.rb
@@ -19,43 +19,4 @@ RSpec.describe App do
       end
     end
   end
-
-  describe 'Section' do
-    let(:subject_class) { Section }
-
-    describe 'let' do
-      it 'provides a getter and setter' do
-        app = subject_class.new
-        app.let(:counter) { 3 }
-        expect(app.counter).to eq 3
-      end
-
-      it 'layzy evaluates' do
-        builder = double('builder')
-        app = subject_class.new
-        app.let(:counter) { builder.build }
-
-        allow(builder).to receive(:build)
-        app.counter
-      end
-
-      it 'memoizes even when the returned value is nil' do
-        builder = double('builder', build: nil)
-        app = subject_class.new
-        app.let(:counter) { builder.build }
-
-        allow(builder).to receive(:build).once
-        app.counter
-        app.counter
-      end
-    end
-
-    describe 'section' do
-      it 'gives the another section app base' do
-        app = subject_class.new
-        app.section(:counters) { 3 }
-        expect(app.counters).to be_a(subject_class)
-      end
-    end  
-  end
 end
